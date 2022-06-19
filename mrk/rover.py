c0534993aa98119d8065fab12f814d9c4ea7a7a5
Joe Walker <joewalk102@gmail.com>
2022-06-19 12:24:08 -0700

    Update rover methods for clarity and better functionality.

--- mrk/rover.py
+++ mrk/rover.py
@@ -5,31 +5,41 @@ class Rover:
     def __init__(self, x: int, y: int, orientation: str, planet: Planet):
         self.x = x
         self.y = y
-        self.bearing = orientation
+        self.current_bearing = orientation
         self.planet = planet
 
     def _action_turn(self, delta: str) -> bool:
-        self.bearing = delta
+        # No checks needed, perform turn.
+        self.current_bearing = delta
         return True
 
     def _action_move(self, delta: list) -> bool:
+        # Get coordinates of the proposed move.
         next_x, next_y = self.planet.get_coordinates(self.x + delta[0], self.y + delta[1])
-        if not self.planet.check_obstacle(next_x, next_y):
-            self.x = next_x
-            self.y = next_y
-            return True
-        return False
+        # Check for obstacles at the new location.
+        if self.planet.check_obstacle(next_x, next_y):
+            return False
+        # If there is no obstacle, move.
+        self.x = next_x
+        self.y = next_y
+        return True
 
-    def _decode(self, command_key: str) -> bool:
+    def _decode(self, command_key: str) -> tuple:
         command = self.commands[command_key]
-        delta = self.movement_keys[self.bearing][command_key]
-        return command(self, delta)
+        delta = self.movement_keys[self.current_bearing][command_key]
+        return command, delta
 
-    def move(self, commands: list) -> bool:
-        for command in commands:
-            if isinstance(command, str):
-                command = command.upper()
-                return self._decode(command)
+    def move(self, command_list: list) -> bool:
+        if set(command_list) - {"F", "B", "L", "R"}:
+            # There should be nothing in `command_list` that is not
+            # in the second set. Only "FBLR" commands are supported.
+            return False
+        for command_key in command_list:
+            command_key = command_key.upper()
+            command_fn, delta =  self._decode(command_key)
+            if not command_fn(self, delta):
+                return False
+        return True
 
     # commands to dispatch for each move key
     commands = {
